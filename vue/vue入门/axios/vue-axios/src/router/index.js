23582fe8b8088b46d4f8f1197afca88b3a0be3e9
huanglongxiang <2714161480@qq.com>
2019-12-12 18:31:03 +0800

    axios初始

---
+++ vue/vue入门/axios/vue-axios/src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/2-2',
+    name: '2-2',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/2-2.vue')
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ vue/vue入门/axios/vue-axios/src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  created(){
+    axios.get('/data.json',{
+      params:{
+        id:12
+      }
+    }).then(res=>{
+      console.log(res);
+    });
+    axios.get({
+      method:'get',
+      url:'/data.json',
+      params:{
+        id:12
+      }
+    }).then(res=>{
+      console.log(res);
+    })
+  }
+}
+</script>
